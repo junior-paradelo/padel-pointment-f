ff6c90151ef8dfde4cfafbd74873d75bd6caa69a
Junior Paradelo <juniorparadelo@gmail.com>
2025-04-27 12:06:46 +0200

    feat: create hero component

--- src/app/page.js
+++ src/app/page.js
@@ -1,76 +1,15 @@
-import { Star } from "lucide-react";
-
 import { CourtCard } from "@/components/court/card";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-
 import { getCourts } from "@/components/court/court.api";
-
 import { Header } from "@/components/header/header";
+import { Hero } from "@/components/hero/hero";
 
 export default async function Home() {
     const courts = await getCourts();
-    const props = {
-        heading: "your go-to platform for booking padel courts",
-        description:
-            "Find available courts near you, check prices, and book your next game easily. Enjoy a seamless booking experience and get ready to play!",
-        reviews: {
-            count: 200,
-            rating: 5.0,
-            avatars: [
-                {
-                    src: "/images/avatar1.webp",
-                    alt: "Avatar 1",
-                },
-                {
-                    src: "/images/avatar2.webp",
-                    alt: "Avatar 2",
-                },
-                {
-                    src: "/images/avatar3.webp",
-                    alt: "Avatar 3",
-                },
-                {
-                    src: "/images/avatar4.webp",
-                    alt: "Avatar 4",
-                },
-                {
-                    src: "/images/avatar5.webp",
-                    alt: "Avatar 5",
-                },
-            ],
-        },
-    };
+
     return (
         <>
             <Header />
-            <section className="py-32">
-                <div className="container text-center">
-                    <div className="mx-auto flex max-w-screen-lg flex-col gap-6">
-                        <h1 className="text-3xl font-extrabold lg:text-6xl">{props.heading}</h1>
-                        <p className="text-balance text-muted-foreground lg:text-lg">{props.description}</p>
-                    </div>
-                    <div className="mx-auto mt-10 flex w-fit flex-col items-center gap-4 sm:flex-row">
-                        <span className="mx-4 inline-flex items-center -space-x-4">
-                            {props.reviews.avatars.map((avatar, index) => (
-                                <Avatar key={index} className="size-14 border">
-                                    <AvatarImage src={avatar.src} alt={avatar.alt} />
-                                </Avatar>
-                            ))}
-                        </span>
-                        <div>
-                            <div className="flex items-center gap-1">
-                                {[...Array(5)].map((_, index) => (
-                                    <Star key={index} className="size-5 fill-yellow-400 text-yellow-400" />
-                                ))}
-                                <span className="mr-1 font-semibold">{props.reviews.rating?.toFixed(1)}</span>
-                            </div>
-                            <p className="text-left font-medium text-muted-foreground">
-                                from {props.reviews.count}+ reviews
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Hero />
             <section id="courts" className="p-6">
                 <div className="flex flex-wrap gap-4 items-start justify-center min-h-screen bg-background">
                     {courts.map((court) => (
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,67 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Star } from "lucide-react";
+
+function Hero() {
+    const props = {
+        heading: "your go-to platform for booking padel courts",
+        description:
+            "Find available courts near you, check prices, and book your next game easily. Enjoy a seamless booking experience and get ready to play!",
+        reviews: {
+            count: 200,
+            rating: 5.0,
+            avatars: [
+                {
+                    src: "/images/avatar1.webp",
+                    alt: "Avatar 1",
+                },
+                {
+                    src: "/images/avatar2.webp",
+                    alt: "Avatar 2",
+                },
+                {
+                    src: "/images/avatar3.webp",
+                    alt: "Avatar 3",
+                },
+                {
+                    src: "/images/avatar4.webp",
+                    alt: "Avatar 4",
+                },
+                {
+                    src: "/images/avatar5.webp",
+                    alt: "Avatar 5",
+                },
+            ],
+        },
+    };
+    return (
+        <section className="py-32">
+            <div className="container text-center">
+                <div className="mx-auto flex max-w-screen-lg flex-col gap-6">
+                    <h1 className="text-3xl font-extrabold lg:text-6xl">{props.heading}</h1>
+                    <p className="text-balance text-muted-foreground lg:text-lg">{props.description}</p>
+                </div>
+                <div className="mx-auto mt-10 flex w-fit flex-col items-center gap-4 sm:flex-row">
+                    <span className="mx-4 inline-flex items-center -space-x-4">
+                        {props.reviews.avatars.map((avatar, index) => (
+                            <Avatar key={index} className="size-14 border">
+                                <AvatarImage src={avatar.src} alt={avatar.alt} />
+                            </Avatar>
+                        ))}
+                    </span>
+                    <div>
+                        <div className="flex items-center gap-1">
+                            {[...Array(5)].map((_, index) => (
+                                <Star key={index} className="size-5 fill-yellow-400 text-yellow-400" />
+                            ))}
+                            <span className="mr-1 font-semibold">{props.reviews.rating?.toFixed(1)}</span>
+                        </div>
+                        <p className="text-left font-medium text-muted-foreground">
+                            from {props.reviews.count}+ reviews
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+export { Hero };
